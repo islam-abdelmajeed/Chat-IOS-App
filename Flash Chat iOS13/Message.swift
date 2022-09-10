de283c41f71aece7ab9e709eb00e69044ea593ed
Islam Khalil <islam.abdelmajeed2@gmail.com>
2022-09-10 12:07:11 +0200

    finish flash-chat proj.

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Islam on 08/10/2021.
+//  Copyright © 2021 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender : String
+    let body : String
+}
